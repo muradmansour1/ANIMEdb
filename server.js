0527d44d5f3fcb9e96130ef2241003411d218b8d
muradmansour <muradmansour2002@gmail.com>
2024-01-12 09:43:01 -0500

    acao

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ const corsOptions = {
   credentials: true, // This is important for credentials mode 'include'
 };
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 // app.get("/api/title/:id", (req, res)=>{
 //   res.json({
 //     count: req.params.id,
