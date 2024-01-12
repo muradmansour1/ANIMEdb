f73e7c1de3ad5bb32ab248e61d76a83e8c742dbd
muradmansour <muradmansour2002@gmail.com>
2024-01-12 15:34:53 -0500

    fixing cors

--- server.js
+++ server.js
@@ -17,18 +17,18 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 const corsOptions = {
-  origin: '*', // Replace with your front-end's origin
+  origin: ['*', 'http://127.0.0.1:5500'],// Replace with your front-end's origin
   credentials: true, // This is important for credentials mode 'include'
 };
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 // app.get("/api/title/:id", (req, res)=>{
 //   res.json({
 //     count: req.params.id,
