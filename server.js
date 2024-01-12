e7b2f0d245a09306bf9a0cb1e538d41c38154911
muradmansour <muradmansour2002@gmail.com>
2024-01-12 15:50:24 -0500

    cors fix pt3

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 // app.get("/api/title/:id", (req, res)=>{
 //   res.json({
 //     count: req.params.id,
